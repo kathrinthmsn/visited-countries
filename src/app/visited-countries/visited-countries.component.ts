d883d353360858d1cf665353c503b7f0c70aa5ef
Kathrin <85406475+kathrinthmsn@users.noreply.github.com>
2022-10-25 11:14:23 +0200

    add visited countries to new column and show count

---
+++ src/app/visited-countries/visited-countries.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CountryService } from '../country.service';
+import * as $ from "jquery";
+
+@Component({
+  selector: 'app-visited-countries',
+  templateUrl: './visited-countries.component.html',
+  styleUrls: ['./visited-countries.component.scss']
+})
+export class VisitedCountriesComponent implements OnInit {
+   @Input() numberVisited: number=0;
+   @Input() visitedC =[];
+
+  constructor(public cs:CountryService) { }
+
+  ngOnInit(): void {
+  }
+
+
+  showNumber(){
+    this.numberVisited= document.querySelectorAll('input[type="checkbox"]:checked').length;
+  }
+  getValue(){
+    var visited =[];
+    
+    $('input[type="checkbox"]:checked').each(function(){
+      visited.push($(this).val());
+    });
+    this.visitedC = visited;  
+    }
+    
+}
+
