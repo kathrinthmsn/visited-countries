f344572c2e83807ed77bff9d002616f56c95d546
Kathrin <85406475+kathrinthmsn@users.noreply.github.com>
2022-10-26 11:09:45 +0200

    added leaflet map

--- src/app/interactive-map/interactive-map.component.ts
+++ src/app/interactive-map/interactive-map.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import  * as L from 'leaflet';
+import 'mapbox-gl-leaflet';
 
 @Component({
   selector: 'app-interactive-map',
   templateUrl: './interactive-map.component.html',
   styleUrls: ['./interactive-map.component.scss']
 })
-export class InteractiveMapComponent implements OnInit {
+export class InteractiveMapComponent implements OnInit, AfterViewInit {
+
+  private map: L.Map;
+
+  @ViewChild('map')
+  private mapContainer: ElementRef<HTMLElement>;
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  ngAfterViewInit() {
+      var map = L.map('map').setView([0, 0], 1);
+      L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=dyQmlIJcLTxhQ3N3uubc', {
+    }).addTo(map); 
   }
 
+  
+
 }
+
